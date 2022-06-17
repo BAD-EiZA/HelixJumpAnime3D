a194b28a4910598b480fd91d86932d1a7f47752e
DESKTOP-BF1CS5G\EiZA <erzhaamierra@gmail.com>
2022-06-18 02:03:15 +0700

    First Commit

---
+++ Assets/CameraCon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    public Transform target;
+    public Transform bg;
+    private float size;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/LoadManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void stage1()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+    public void stage2()
+    {
+        SceneManager.LoadScene("Stage2");
+    }
+    public void stage3()
+    {
+        SceneManager.LoadScene("Stage3");
+    }
+    public void stage4()
+    {
+        SceneManager.LoadScene("Stage4");
+    }
+    public void stage5()
+    {
+        SceneManager.LoadScene("Stage5");
+    }
+    public void homes()
+    {
+        SceneManager.LoadScene("MainMenus");
+    }
+    public void quittt()
+    {
+        Application.Quit();
+    }
+}
